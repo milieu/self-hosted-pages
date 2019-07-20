b5f428cd868287cc3af96b7d451920156856b5da
Meredith Leu <meredith.leu@gmail.com>
2019-07-20 09:53:56 -0700

    Updates

---
+++ precache-manifest.4c15c25b7b562fde21772de5a980c3a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc7b5ad72f136ccbb51989388e5a62a",
+    "url": "/self-hosted-demo/index.html"
+  },
+  {
+    "revision": "7543a050a332c28c77af",
+    "url": "/self-hosted-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e27e07f6c45b9b65289f",
+    "url": "/self-hosted-demo/static/js/2.b7bd9ff7.chunk.js"
+  },
+  {
+    "revision": "7543a050a332c28c77af",
+    "url": "/self-hosted-demo/static/js/main.37d29629.chunk.js"
+  },
+  {
+    "revision": "45cc9789ad7bf631d8f1",
+    "url": "/self-hosted-demo/static/js/runtime~main.d967db8b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/self-hosted-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
